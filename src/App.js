26c4da80e1415edee570a4ba7455714800a8a540
Color Codex <colorcodex8@Colors-iMac.local>
2023-09-03 10:37:21 -0500

    building the interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState, useEffect, useCallback } from "react";
 import { WiDayHaze } from "react-icons/wi";
-import { BiTrash } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
-import appointmentList from "./data.json";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointmentList(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container font-thin mx-auto mt-3">
       <h1 className="text-5xl mb-3">
@@ -12,33 +26,15 @@ function App() {
         My Appointments
       </h1>
       <AddAppointment />
-      <Search />
+      <Search
+      // query={query}
+      // onQueryChange={(myQuery) => setQuery(myQuery)}
+      // orderBy={orderBy}
+      />
 
       <ul className="divide-y divide-gray-400">
         {appointmentList.map((appointment) => (
-          <li className="px-3 py-3 flex items-start">
-            <button
-              type="button"
-              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <BiTrash />
-            </button>
-            <div className="flex-grow">
-              <div className="flex items-center">
-                <span className="flex-none font-medium text-2xl text-blue-500">
-                  {appointment.petName}
-                </span>
-                <span className="flex-grow text-right">
-                  {appointment.aptDate}
-                </span>
-              </div>
-              <div>
-                <b className="font-bold text-blue-500">Owner:</b>{" "}
-                {appointment.ownerName}
-              </div>
-              <div className="leading-tight">{appointment.aptNotes}</div>
-            </div>
-          </li>
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
         ))}
       </ul>
     </div>
